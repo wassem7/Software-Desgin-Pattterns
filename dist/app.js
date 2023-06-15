8263be4b24fa659afe7e2e124553c77f32ca28fb
wassem7 <waseemdarkwa@gmail.com>
2023-06-15 13:13:48 +0100

    Template method pattern

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,27 @@
 "use strict";
-console.log('yo');
+class Television {
+    powerOn() {
+        return 'Powering On..';
+    }
+    powerOff() {
+        return 'Powering Off..';
+    }
+    channelUp() {
+        return 'Channel Up...';
+    }
+    channelDown() {
+        return 'Channel Down...';
+    }
+}
+class Samsung extends Television {
+    constructor() {
+        super(...arguments);
+        this.properties = {
+            width: '50in',
+            height: '50in',
+        };
+    }
+}
+const tv = new Samsung();
+console.log(tv.properties.height);
+console.log(tv.powerOn());
